7150881862fcbb52b9b4eae7f50d2309d4684a56
Franklin Byaruhanga <byaruhaf@protonmail.com>
2026-05-12 16:41:30 +0300

    Refine parallel session layout conditions for compact size classes
    Only use compactParallelLayout when multiple content IDs are present, while simplifying redundant size class checks and defaulting remaining cases to horizontalLayout.

--- MythConf/MythConf26/Programme/ParallelSessionsRowView.swift
+++ MythConf/MythConf26/Programme/ParallelSessionsRowView.swift
@@ -18,18 +18,10 @@ struct ParallelSessionsRowView: View {
         Group {
             if dynamicTypeSize.isAccessibilitySize {
                 accessibilityLayout
-            } else if verticalSizeClass == .regular && horizontalSizeClass == .compact {
-                // iPhone Portrait or iPad 1/3 split view for Multitasking for instance
+            } else if horizontalSizeClass == .compact
+                && verticalSizeClass == .regular
+                && session.contentIDs.count > 1 {
                 compactParallelLayout
-            } else if verticalSizeClass == .compact && horizontalSizeClass == .compact  && session.contentIDs.count > 1{
-                // some "standard" iPhone Landscape (iPhone SE, X, XS, 7, 8, ...)
-                horizontalLayout
-            } else if verticalSizeClass == .compact && horizontalSizeClass == .regular  && session.contentIDs.count > 1{
-                // some "bigger" iPhone Landscape (iPhone Xs Max, 6s Plus, 7 Plus, 8 Plus, ...)
-                horizontalLayout
-            } else if verticalSizeClass == .regular && horizontalSizeClass == .regular  && session.contentIDs.count > 1{
-                // macOS or iPad without split view - no Multitasking
-                horizontalLayout
             } else {
                 horizontalLayout
             }
