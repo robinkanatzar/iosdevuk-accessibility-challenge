7e38c17bcf4bd1ee4c9a29f395e59f79917789f0
steven-hill <stevenhill15@icloud.com>
2026-05-13 18:37:31 +0100

    Make `SessionDetailView` adapt to dynamic type sizes on iPhone and iPad, in portrait and landscape

--- MythConf/MythConf26/Programme/SessionDetailView.swift
+++ MythConf/MythConf26/Programme/SessionDetailView.swift
@@ -7,16 +7,19 @@ import SwiftUI
 
 struct SessionDetailView: View {
     @Environment(ViewModel.self) private var viewModel
+    @Environment(\.dynamicTypeSize) private var dynamicTypeSize
+    @Environment(\.horizontalSizeClass) private var horizontalSizeClass
     let talkReference: TalkReference
-
+    
     private var talk: Talk { viewModel.talkFrom(talkID: talkReference.talkID) }
     private var session: Session { talkReference.session }
-
+    
     var body: some View {
         ScrollView {
             VStack(alignment: .leading) {
                 // Time and location
-                HStack {
+                let layout = (horizontalSizeClass == .compact || UIDevice.current.userInterfaceIdiom == .pad) && dynamicTypeSize >= .accessibility3 ? AnyLayout(VStackLayout(alignment: .leading)) : AnyLayout(HStackLayout(alignment: .top))
+                layout {
                     Label(session.timeRange, systemImage: "clock")
                     Spacer()
                     NavigationLink(value: LocationNavigationID(value: talk.locationID)) {
@@ -26,7 +29,7 @@ struct SessionDetailView: View {
                 .font(.subheadline)
                 .foregroundStyle(.secondary)
                 .padding(.bottom)
-
+                
                 // Speakers
                 ForEach(talk.speakerIDs, id: \.self) { speakerID in
                     NavigationLink(value: SpeakerNavigationID(value: speakerID)) {
@@ -34,10 +37,10 @@ struct SessionDetailView: View {
                     }
                     .buttonStyle(.plain)
                 }
-
+                
                 Divider()
                     .padding(.vertical)
-
+                
                 // Abstract
                 Text(talk.talkDescription)
             }
