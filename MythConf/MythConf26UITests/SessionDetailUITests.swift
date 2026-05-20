60f5006d6041ad79a76af50694aeec5d07c4214b
Franklin Byaruhanga <byaruhaf@protonmail.com>
2026-05-20 16:37:04 +0300

    Fix UI test for session details

--- MythConf/MythConf26UITests/SessionDetailUITests.swift
+++ MythConf/MythConf26UITests/SessionDetailUITests.swift
@@ -18,7 +18,8 @@ final class SessionDetailUITests: MythConfUITestCase {
 
         XCTAssertTrue(app.navigationBars["Session Details"].waitForExistence(timeout: 5))
         XCTAssertTrue(element(identifier: "sessionDetail.title").waitForExistence(timeout: 5))
-        XCTAssertTrue(element(identifier: "sessionDetail.time").waitForExistence(timeout: 5))
+        let aboutHeading = element(identifier: "sessionDetail.aboutHeading")
+        XCTAssertTrue(aboutHeading.waitForExistence(timeout: 5))
         XCTAssertTrue(element(identifier: "sessionDetail.location").waitForExistence(timeout: 5))
         XCTAssertTrue(element(identifier: "sessionDetail.about").waitForExistence(timeout: 5))
         XCTAssertTrue(element(identifier: "sessionDetail.favouriteAction").waitForExistence(timeout: 5))
